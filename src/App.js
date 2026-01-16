7714136a1b432345ab36aa770f1dea7ea27a3d3c
salatesxutpblu <ilyakanevets@gmail.com>
2026-01-16 10:42:42 +0300

    asdas
    sadsda

--- src/App.js
+++ src/App.js
@@ -1,6 +1,68 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [role, setRole] = useState('user')
+  const [users, setUsers] = useState([])
+
+  const deleteUser = event => {
+    let a = window.confirm('Вы уверены что хотите удалить пользователя?')
+    if (a === false) {
+      return
+    }
+      setUsers(users.filter(user => user.id != event.target.value))
+   }
+
+  const handleChangeUsername = event => {
+    setUsername(event.target.value)
+  }
+  const handleChangeEmail = event => {
+    setEmail(event.target.value)
+  }
+  const handleChangeRole = event => {
+    setRole(event.target.value)
+  }
+
+  const addUser = event => {
+    let oldUsers = users
+    let result = false
+    for (let a = 0; a < email.length; a++) {
+      if (email[a] === '@') {
+        result = true
+      }
+    }
+    if (username.length > 15) {
+      alert('Username не может быть таким длинным!')
+      return
+    }
+    if (result === false) {
+      alert('Email должен содержать @')
+      return
+    }
+    if (username === '') {
+      alert('Заполните username')
+      return
+    }
+    if (email === '') {
+      alert('Заполните email')
+      return
+    }
+    let cleanUsername = document.getElementById('username')
+    let cleanEmail = document.getElementById('email')
+    let cleanRole = document.getElementById('selectRole')
+    cleanUsername.value = ''
+    cleanEmail.value = ''
+    cleanRole.value = ''
+    let id = Math.ceil(Math.random() * 2000)
+    setUsers(oldUsers => [...oldUsers, {
+      username: username,
+      email: email,
+      role: role,
+      id: id
+    }])
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -15,27 +77,21 @@ function App() {
           </tr>
           </thead>
           <tbody>
-          <tr>
-            <td><h1>234324</h1></td>
-            <td><h1>Чел</h1></td>
-            <td><h1>bvnvnb@gmail.com</h1></td>
-            <td><h1>user</h1></td>
-            <td className='text-center'><button className='btn btn-danger btn-lg'>Удалить</button></td>
-          </tr>
-          <tr>
-            <td><h1>234324</h1></td>
-            <td><h1>Чел</h1></td>
-            <td><h1>bvnvnb@gmail.com</h1></td>
-            <td><h1>user</h1></td>
-            <td className='text-center'><button className='btn btn-danger btn-lg'>Удалить</button></td>
-          </tr>
-          <tr>
-            <td><h1>234324</h1></td>
-            <td><h1>Чел</h1></td>
-            <td><h1>bvnvnb@gmail.com</h1></td>
-            <td><h1>user</h1></td>
-            <td className='text-center'><button className='btn btn-danger btn-lg'>Удалить</button></td>
-          </tr>
+              {(() => {
+                let users2 = []
+                for (let a = 0; a < users.length; a++) {
+                  users2.push(
+                    <tr key={a}>
+                  <td><h2>{users[a].id}</h2></td>
+                  <td><h2>{users[a].username}</h2></td>
+                  <td><h2>{users[a].email}</h2></td>
+                  <td><h2>{users[a].role}</h2></td>
+                  <td className='text-center'><button value={users[a].id} onClick={deleteUser} className='btn btn-danger btn-lg'>Удалить</button></td>
+                    </tr>
+                  );
+                }
+                return users2;
+              })()}            
           </tbody>
         </table>
       </header>
@@ -44,20 +100,19 @@ function App() {
       <div class="input-group-prepend">
           <span class="input-group-text" id="basic-addon1">@</span>
       </div>
-          <input required type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+          <input onChange={handleChangeUsername} required type="text" id='username' class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
           <span class="input-group-text" id="basic-addon1">@</span>
       </div>
-          <input required type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"></input>
+          <input onChange={handleChangeEmail} id='email' required type="text" class="form-control email" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"></input>
     </div>
-      <div></div>
-      <select className='form-select'>
+      <select id='selectRole' onChange={handleChangeRole} className='form-select'>
         <option value="user">User</option>
         <option value="admin">Admin</option>
       </select>
-      <button type="button" class="btn btn-primary btn-lg mt-2">Primary</button>
+      <button onClick={addUser} type="submit" class="btn btn-primary btn-lg mt-2">Добавить пользователя</button>
     </div>
     
   );
